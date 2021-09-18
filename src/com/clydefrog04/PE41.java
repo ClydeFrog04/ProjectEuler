9b76d636586c30fa52e6daa6795e3d3ba2ce3fc7
ClydeFrog04 <egan.randal@gmail.com>
2021-09-18 12:28:21 -0600

    Cleaned up PE41

--- src/com/clydefrog04/PE41.java
+++ src/com/clydefrog04/PE41.java
@@ -4,13 +4,6 @@ import com.clydefrog04.PEUtils.NumUtil;
 
 public class PE41 {
 
-    /*
-
-
-We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once.
-For example, 2143 is a 4-digit pandigital and is also prime.
-What is the largest n-digit pandigital prime that exists?
-     */
     public static void main(String[] args) {
         //limit can be 987654321 since that is the largest possible pandigital number
         int limit = 987654321;
@@ -20,10 +13,9 @@ What is the largest n-digit pandigital prime that exists?
         start = System.currentTimeMillis();
         for (int i = 3; i < limit; i++) {
             boolean isPP = numUtil.improvedPan(i) && numUtil.isPrime(i);
-            if(isPP && i > maxPP) maxPP = i;
+            if (isPP && i > maxPP) maxPP = i;
         }
         end = System.currentTimeMillis();
-
-        System.out.printf("Largest found pandigital prime was: %d%n Time taken: %ds%n", maxPP, (end - start)/1000);
+        System.out.printf("Largest found pandigital prime was: %d%n Time taken: %ds%n", maxPP, (end - start) / 1000);
     }
 }
